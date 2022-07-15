a0fc66f53a591e0e5b079aa8b86f342929247c92
HP <sachdevamohita5@gmail.com>
2022-07-15 15:09:27 +0530

    uc3

--- Tic-Tac-Toe/src/com/bridgelap/TicTacToeGame.java
+++ Tic-Tac-Toe/src/com/bridgelap/TicTacToeGame.java
@@ -7,6 +7,11 @@ public class TicTacToeGame {
     static char[] board= new char[10];
     static Scanner src = new Scanner(System.in);
     static char symbol;
+    static char computerChoice;
+    static char playerChoice;
+    static char choice;
+
+    static int position;
     static void createBoard(char[] board){
         for(int i=1; i<board.length; i++){
             board[i]='-';
@@ -35,12 +40,74 @@ public class TicTacToeGame {
             return 'x';
         }
     }
+    static void updateBoard(char board[], int player, int position) {
+        if (player == 1) {
+            symbol = playerChoice();
+        } else {
+            symbol = computerChoice();
+        }
+        switch (position) {
+            case 1:
+                board[1] = symbol;
+                break;
+            case 2:
+                board[2] = symbol;
+                break;
+            case 3:
+                board[3] = symbol;
+                break;
+            case 4:
+                board[4] = symbol;
+                break;
+            case 5:
+                board[5] = symbol;
+                break;
+            case 6:
+                board[6] = symbol;
+                break;
+            case 7:
+                board[7] = symbol;
+                break;
+            case 8:
+                board[8] = symbol;
+                break;
+            case 9:
+                board[9] = symbol;
+                break;
+            default:
+                break;
+        }
+    }
+
+    static void playerMove(char[] board) {
+        System.out.println("Please make a move in between 1 to 9");
+        position = src.nextInt();
+
+        updateBoard(board, 1, position);
+        System.out.println("Player moved to position " + position);
+    }
+
+    static void computerMove(char[] board) {
+        position = (int) ((Math.random() * 10) % 9) + 1;
+        updateBoard(board, 2, position);
+        System.out.println("Computer moved to position " + position);
+    }
+
 
     public static void main(String[] args) {
-        System.out.println(" welcome to the tic tac toe game");
-        createBoard(board);
-        showboard(board);
-        System.out.println("Player Choice is "+playerChoice());
-        System.out.println("Computer Choice is "+computerChoice());
+        do {
+            createBoard(board);
+
+            playerChoice = playerChoice();
+            computerChoice = computerChoice();
+
+            showboard(board);
+            playerMove(board);
+            showboard(board);
+            computerMove(board);
+            showboard(board);
+            System.out.println("Do you want to Continue(y/n)");
+            choice=src.next().charAt(0);
+        }while(choice == 'y');
     }
 }
